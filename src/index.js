9f2242bba96a2812a06fb313b1f7f80d30d85a68
Mönch,Priscilla P.P.I.M <p.monch@student.fontys.nl>
2019-05-15 14:22:31 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const HOST = '0.0.0.0';
+const PORT = '8080';
+app.get('/', (req, res) => res.send('Hello Cool'));
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
